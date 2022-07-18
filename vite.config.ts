206921e85b78a48aef15041c5cf472b532c4664a
Anthony Hubble <anthony@madewithply.com>
2022-07-18 22:03:19 +1000

    Remove hash from compiled name

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 })
